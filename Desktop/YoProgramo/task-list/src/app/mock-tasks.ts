bec6b774400dc14b5e62fead845156ded9d80af8
Maiakr <mushymai@hotmail.com>
2022-09-30 19:29:24 -0300

    servicio y servidor de tareas listo

--- Desktop/YoProgramo/task-list/src/app/mock-tasks.ts
+++ Desktop/YoProgramo/task-list/src/app/mock-tasks.ts
@@ -1,27 +1,33 @@
+import { faLessThanEqual } from "@fortawesome/free-solid-svg-icons"
 import {Task} from "./Task"
 export const TASKS: Task []= [
     {
         id: 1,
         text: "Terminar tercer módulo de Angular",
-        day: 'Septiembre 30 a las 18:00',
-        reminder: true,
+        day: 'Septiembre 30 a las 20:00',
+        reminder: true
+      },
+      {id: 2,
+      text: "Hacer las compras para el almuerzo",
+      day: 'Septiembre 30 a las 12:00',
+      reminder: false
+      },
+     {
+      id: 3,
+      text: "investigar sobre Bootstrap",
+      day: 'Septiembre 30 a las 22:00',
+      reminder: false
     },
     {
-        id: 2,
-        text: "Hacer compras para almuerzo",
-        day: 'Septiembre 30 a las 12:00',
-        reminder: true,
+      id: 4,
+      text: "ir a jugar al handball",
+      day: 'Septiembre 30 a las 21:00',
+      reminder: true
     },
     {
-        id: 3,
-        text: "Investigar sobre Bootstrap",
-        day: 'Septiembre 30 a las 14:00',
-        reminder: false,
-    },
-    {
-        id: 4,
-        text: "Ir a jugar al handball",
-        day: 'Septiembre 30 a las 21:00',
-        reminder: false,
-    },
+      id: 5,
+      text: "Ir a correr",
+      day: 'Septiembre 30 a las 8:00',
+      reminder: false
+    }
 ]
